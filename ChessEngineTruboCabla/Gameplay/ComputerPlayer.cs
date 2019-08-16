39aea5b9c49353adb71c63e840ac2216174a25a7
jack.giannini@gmail.com <jack.giannini@gmail.com>
2019-08-15 21:23:59 -0400

    checking in

--- ChessEngineTruboCabla/Gameplay/ComputerPlayer.cs
+++ ChessEngineTruboCabla/Gameplay/ComputerPlayer.cs
@@ -15,24 +15,7 @@ namespace ChessEngineTruboCabla.Gameplay
 
         public override string GetInput(Board board)
         {
-            //return "";
-            string bestMove = "";
-            int bestMoveValue = 0;
-            board.GenerateAllAlgebraicAvailableMoves();
-            for(int i=0; i<board.AlgebraicAvailableMoves.Count; i++)
-            {
-                Board hypotheticalBoard = Utilities.DeepClone<Board>(board);
-                hypotheticalBoard.MakeMove(board.AlgebraicAvailableMoves[i]);
-                int evaluation = Evaluation.EvaluateBoard(hypotheticalBoard);
-                if (evaluation > bestMoveValue)
-                {
-                    bestMove = board.AlgebraicAvailableMoves[i];
-                    bestMoveValue = evaluation;
-                }
-            }
-            //List<string> moves = board.AlgebraicAvailableMoves;
-            //Random r = new Random();
-            //return moves[r.Next(0,moves.Count)];
+            string bestMove = MoveGenerator.MakeBestMove(board, 1);
             return bestMove;
         }
     }
---
+++ ChessEngineTruboCabla/Gameplay/MoveGenerator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessEngineTruboCabla.Gameplay
+{
+    public static class MoveGenerator
+    {
+        public static string MakeBestMove(Board board, int ply)
+        {
+            string bestMove = "";
+            int bestMoveValue = Evaluation.EvaluateBoard(board);
+            board.GenerateAllAlgebraicAvailableMoves();
+            for (int i = 0; i < board.AlgebraicAvailableMoves.Count; i++)
+            {
+                Board hypotheticalBoard = Utilities.DeepClone<Board>(board);
+                hypotheticalBoard.MakeMove(board.AlgebraicAvailableMoves[i]);
+                int evaluation = Evaluation.EvaluateBoard(hypotheticalBoard);
+                if (board.Turn == 1)
+                {
+                    if (evaluation >= bestMoveValue)
+                    {
+                        bestMove = board.AlgebraicAvailableMoves[i];
+                        bestMoveValue = evaluation;
+                    }
+                }
+                else if (board.Turn == -1)
+                {
+                    if (evaluation <= bestMoveValue)
+                    {
+                        bestMove = board.AlgebraicAvailableMoves[i];
+                        bestMoveValue = evaluation;
+                    }
+                }
+            }
+            return bestMove;
+        }
+
+        public static string SearchPlyForBestMove(Board board, int depth, int maximizingPlayer)
+        {
+            string bestMove = "";
+            int bestMoveValue = Evaluation.EvaluateBoard(board);
+            int color = board.Turn;
+
+            if(depth == 0)
+            {
+
+            }
+
+            board.GenerateAllAlgebraicAvailableMoves();
+            for (int i = 0; i < board.AlgebraicAvailableMoves.Count; i++)
+            {
+                Board hypotheticalBoard = Utilities.DeepClone<Board>(board);
+                hypotheticalBoard.MakeMove(board.AlgebraicAvailableMoves[i]);
+                int evaluation = Evaluation.EvaluateBoard(hypotheticalBoard);
+
+            }
+
+
+
+
+
+            return bestMove;
+        }
+
+    }
+}
